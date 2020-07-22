33fddf2c4366bbe381f2656461cbc17c742b287d
Evan <evancole82590@gmail.com>
2020-07-22 11:23:52 -0400

    Remove build dir in .gitignore

---
+++ client/tibia-azure-example/build/precache-manifest.e39703e8fb77bf31c82db903ae1c32d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5612905d395b5511cd106425c95d51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dd81d976a420751120d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "946f721b7a5d84f128f8",
+    "url": "/static/js/2.2c4f8fc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c4f8fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd81d976a420751120d",
+    "url": "/static/js/main.cb95e7d5.chunk.js"
+  },
+  {
+    "revision": "cc8952209ef4c2ade906",
+    "url": "/static/js/runtime-main.b32fd800.js"
+  }
+]);
